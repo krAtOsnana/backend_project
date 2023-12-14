498a441edbe4d48fc063adcb868ad2ee99cef3d0
krtatos <kkushagra626@gmail.com>
2023-12-14 19:31:56 +0530

    middlewares set

--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
 //require('dotenv').config({path: './env'})
 
 import dotenv from 'dotenv'
-
-import connectDB from "./db/index.js";
-connectDB();
-
 dotenv.config({
     path:'./env'
 })
+
+import connectDB from "./db/index.js";
+connectDB().then(() => {
+    
+    app.on("error",() => {
+        console.log(`on Listening Error: ${error}`);
+        throw error;
+    })
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`server is running on port: ${process.env.PORT}`);
+    }) 
+}).catch((err) => {
+    console.log("mongodb connection error!!!",err);
+})
+
+
 
 
 
