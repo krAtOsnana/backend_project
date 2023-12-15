65ba553a06aa85065f90ac9a12aa6cb99f1d50f8
krtatos <kkushagra626@gmail.com>
2023-12-15 09:30:29 +0530

    completing methods for refresh and access tokens

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,5 +1,5 @@
 import mongoose, {Schema} from 'mongoose'
-import jsonWebToken from 'jsonwebtoken'
+import jwt from 'jsonwebtoken'
 import bcrypt from 'bcrypt'
 
 const userSchema = new Schema({
@@ -52,8 +52,40 @@ const userSchema = new Schema({
 userSchema.pre("save", async function(next){
     if(!this.isModified("password")) return next(); 
 
-    this.password = bcrypt.hash(this.password, 7)
+    this.password = bcrypt.hash(this.password, 10)
     next()
 })
+
+userSchema.methods.isPasswordCorrect = async function (password){
+    return await bcrypt.compare(password, this.password)
+}
+
+userSchema.methods.generateAccessToken = function (){
+    return jwt.sign(
+    {
+        _id: this._id,
+        userName: this.userName,
+        fullName: this.fullName,
+        email: this.email,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+        expiresIn: process.env.ACCESS_TOKEN_EXPIARY
+    }
+    )
+}
+userSchema.methods.generateRefreshToken = function (){
+    return jwt.sign(
+        {
+            _id: this._id,    
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIARY
+        }
+        )
+}
+
+
 
 export const User = mongoose.model("User",userSchema)
